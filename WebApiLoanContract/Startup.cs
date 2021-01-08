ccc18e300a1ebba5650652ce14be01e944acecb6
Vanessa Denny <vanessaidenny@hotmail.com>
2021-01-07 22:16:52 -0300

    refactor: Removing unnecessary libraries

--- WebApiLoanContract/Startup.cs
+++ WebApiLoanContract/Startup.cs
@@ -1,16 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using WebApiLoanContract.Data;
 
 namespace WebApiLoanContract
 {
@@ -26,7 +21,7 @@ namespace WebApiLoanContract
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
