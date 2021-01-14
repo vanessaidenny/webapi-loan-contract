e2fd18287958d55dc2abf646ae37b8c8354e7201
Vanessa Denny <vanessaidenny@hotmail.com>
2021-01-14 14:18:36 -0300

    test: Contract Service - Get contracts and return specific data

---
+++ WebApiLoanContract.Tests/ContractServiceTest.cs
@@ -0,0 +1,52 @@
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApiLoanContract.Data;
+using WebApiLoanContract.Services;
+using WebApiLoanContract.Models;
+using Xunit;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiLoanContract.Tests
+{
+    public class ContractServiceTest
+    {
+        /// <summary>
+        /// GET contracts Unit Tests
+        /// </summary>
+        /// <returns> ReturnArrayContracts
+        [Fact]
+        public async Task GetContractsTest_ReturnSpecificData()
+        {
+            //ARRANGE - Create InMemoryDatabase
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: "MockDatabase")
+                .Options;
+
+            //Create mocked Context by seeding Data
+            using (var context = new DataContext(options))
+            {
+                context.Contracts.Add(new Contract
+                {
+                    NumberInstallments = 2,
+                    AmountFinanced = 20
+                });
+                context.SaveChanges();
+            }
+
+            //ACT - Context instance with Data
+            using (var context = new DataContext(options))
+            {
+                ContractService contractService = new ContractService(context);
+                var result = await contractService.GetContracts();
+
+                //ASSERT
+                Assert.NotEmpty(result);
+                foreach (var data in result)
+                {
+                    Assert.Equal(data.NumberInstallments, 2);
+                    Assert.Equal(data.AmountFinanced, 20);
+                }
+            }
+        }
+    }
+}
