ab803110ce766fbd695e1beef93f027d7d26f623
Vanessa Denny <vanessaidenny@hotmail.com>
2021-01-14 14:21:47 -0300

    test: Contract Service - Get contract by id and validate id

--- WebApiLoanContract.Tests/ContractServiceTest.cs
+++ WebApiLoanContract.Tests/ContractServiceTest.cs
@@ -10,10 +10,6 @@ namespace WebApiLoanContract.Tests
 {
     public class ContractServiceTest
     {
-        /// <summary>
-        /// GET contracts Unit Tests
-        /// </summary>
-        /// <returns> ReturnArrayContracts
         [Fact]
         public async Task GetContractsTest_ReturnSpecificData()
         {
@@ -46,6 +42,36 @@ namespace WebApiLoanContract.Tests
                     Assert.Equal(data.NumberInstallments, 2);
                     Assert.Equal(data.AmountFinanced, 20);
                 }
+            }
+        }        
+        
+        [Fact]
+        public async Task GetContractsByIdTest_ValidateContractId1()
+        {
+            //ARRANGE - Create InMemoryDatabase
+            var options = new DbContextOptionsBuilder<DataContext>()
+                .UseInMemoryDatabase(databaseName: "MockDatabase")
+                .Options;
+
+            //Create mocked Context by seeding Data
+            using (var context = new DataContext(options))
+            {
+                context.Contracts.Add(new Contract
+                {
+                    NumberInstallments = 2,
+                    AmountFinanced = 20
+                });
+                context.SaveChanges();
+            }
+
+            //ACT - Context instance with Data
+            using (var context = new DataContext(options))
+            {
+                ContractService contractService = new ContractService(context);
+                var result = await contractService.GetContractsById(1);
+
+                //ASSERT
+                Assert.Equal(result.ContractId, 1);
             }
         }
     }
